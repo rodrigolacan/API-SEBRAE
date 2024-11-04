c5329ef3ab6b66424350f01b9f683efb2b3d33c7
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2024-11-04 11:20:40 -0400

    refact: removendo annotation @componentScan por não ter necessidade

--- src/main/java/rr/sebrae/api/ApiApplication.java
+++ src/main/java/rr/sebrae/api/ApiApplication.java
@@ -1,9 +1,12 @@
 package rr.sebrae.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class ApiApplication extends SpringBootServletInitializer {
